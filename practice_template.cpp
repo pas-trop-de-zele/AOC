e82edf62fc88e629ca3cbd3d1213d5b48f20b1f5
Sam <huy.le09@yahoo.com>
2022-11-29 10:57:28 -0800

    Update

---
+++ practice_template.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vvi vector<vi>
+#define vs vector<string>
+#define pi pair<int, int>
+#define vpi vector<pi>
+
+const vpi MOVES_ADJACENT{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const vpi MOVES_ALL{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+template <typename T>
+void deb(T a) {
+    cout << a << endl;
+}
+
+template <typename T>
+void deb(const vector<T> &v) {
+    for (auto &c : v) {
+        cout << c << " ";
+    }
+    cout << endl;
+}
+
+void deb(int *a, int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void deb(T *a, int n, int m) {
+    for (int r = 0; r < n; ++r) {
+        for (int c = 0; c < m; ++c) {
+            cout << a[r][c] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void solve() {
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+}
