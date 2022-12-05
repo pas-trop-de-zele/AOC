1e554aa01d14ede465e1769426af6c8e2e3f06ae
Sam Le <huy.le09@protonmail.com>
2022-12-04 19:57:31 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import math
+import heapq
+from collections import deque, defaultdict
+
+
+def debM(m):
+    for r in m:
+        for c in r:
+            print(c, end='')
+        print()
+    print()
+
+
+MOVES_ADJACENT = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+MOVES_ALL = [[1, 0], [1, 1], [0, 1], [-1, 1],
+             [-1, 0], [-1, -1], [0, -1], [1, -1]]
+fin = sys.stdin.read().strip().split('\n')
